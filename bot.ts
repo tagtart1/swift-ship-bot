3934b272f1f29152df17174d046cbff5a0461a39
tagtart1 <100038117+tagtart1@users.noreply.github.com>
2026-04-29 16:58:55 -0500

    new command

--- bot.ts
+++ bot.ts
@@ -5,6 +5,9 @@ const bot = new Bot(Deno.env.get("BOT_TOKEN")!);
 // Handle the /start command.
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 bot.command("purchase", (ctx) => ctx.reply("Purchase command"));
+bot.command("matt", (ctx) => ctx.reply("JIMMY JIMMY JIMMY command"));
+bot.command("jimmy", (ctx) => ctx.reply("Matt oh matt oh matt oh"));
+
 // Handle other messages.
 bot.on("message", (ctx) => ctx.reply("Got another message!"));
 
